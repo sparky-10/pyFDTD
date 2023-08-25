01f8215ce9196d92458912e57df59fa5e2fbb689
sparky-10 <70389073+sparky-10@users.noreply.github.com>
2023-08-25 15:01:29 +0100

    More sensible/simple test script

--- testScript.py
+++ testScript.py
@@ -12,51 +12,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+# %% Get number of dimensions as input
+
+import sys
+if len(sys.argv) > 1:
+    NDim = int(sys.argv[1])
+else:
+    NDim = 2
+
 # %% Run pyFDTD
 
 start_time = time.time()
 
-fdtd = pyFDTD(NDim=2, debug=True)
-fdtd.t = 5e-3
-fdtd.newEmptyMesh([400,400])
+fdtd = pyFDTD(NDim=NDim, debug=True)
 
-# Some run settings
-fdtd.plotIthUpdate = 2
+fdtd.t = 10e-3
+fdtd.X = 10e-3
+fdtd.setInputs()
+
+fdtd.plotIthUpdate = 5
 fdtd.doPlot = True
-fdtd.saveRecData = True
+fdtd.saveRecData = False
 fdtd.saveGif = False
 
-# Before run and/or prepareMesh
-fdtd.beta = 0.0
-fdtd.betaBorder = 1.0
-fdtd.threshold = 0.0
-fdtd.betaMode = 'constant'
-# fdtd.betaMode = 'varying'
+if fdtd.NDim == 1:
+    # 1D Mesh
+    fdtd.betaMode = 'varying'
+    fdtd.mesh[0:10] = 1
+    fdtd.mesh[90:] = 0.5
+elif fdtd.NDim == 2:
+    # # 2D mesh
+    # fdtd.newEmptyMesh([200, 300])
+    # fdtd.moveSrc([1.0, 0.75])
+    # fdtd.srcFreq[0] = 1200
+    # Load a 2D image
+    fdtd.betaMode = 'varying'
+    fdtd.betaBorder = 1
+    # fdtd.loadImage('../images/Flat_wall.png')
+    fdtd.loadImage('../images/Binary_amplitude_diffuser.png')
+    fdtd.moveSrc([1.25, 1.25])
+    fdtd.moveRec([1.25, 1.75])
+elif fdtd.NDim == 3:
+    # 3D mesh
+    fdtd.newEmptyMesh([120, 170, 50])
+    # Place a block in the mesh
+    fdtd.mesh[60:100,70:100,20:40] = 1
+    # Move the source
+    fdtd.moveSrc([0.5, 0.6, 0.3])
 
-# fdtd.loadImage('../images/Flat_wall.png')
-# # fdtd.image2Mesh(xMesh=200, yMesh=200);
-# fdtd.moveSrc([fdtd.D[0]*0.5, fdtd.D[1]*0.7],0)
-# fdtd.moveRec([fdtd.D[0]*0.5, fdtd.D[1]*0.85],0)
-
-# # fdtd.newEmptyMesh([50,50,50])
-# # fdtd.setInputs()
-# fdtd.mesh[20:30,20:30,10:40] = 1
-# fdtd.cLims = [-0.02, 0.02]
-# fdtd.moveSrc([fdtd.D[0]*0.3, fdtd.D[1]*0.4, fdtd.D[2]*0.1],0)
-
-
-# fdtd.addSrc([fdtd.X*10, fdtd.X*10, fdtd.X*10])
-
-
-# Run simulation in steps
-# fdtd.runReset()
-# fdtd.checkMesh()
-# fdtd.prepareMesh()
-#for i in range(0,50):
-#    fdtd.runSteps()
-#    print(fdtd.running)
-
-# Run full simulation
+# Run simulation
 fdtd.run()
 
 print("--- %s seconds ---" % (time.time() - start_time))
